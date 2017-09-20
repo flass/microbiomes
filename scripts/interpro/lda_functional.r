be2be3019d4bab63614f2386dd1312aba454c2fc
flass <florentlassalle@hotmail.fr>
2017-09-20 18:16:51 +0100

    clean lda_functional.r

--- scripts/interpro/lda_functional.r
+++ scripts/interpro/lda_functional.r
@@ -7,63 +7,52 @@ library('parallel')
 
 # folder where all data are stored
 cargs = commandArgs(trailingOnly=T)
-if (len(cargs)>0){
-	metaghomedir = cargs[1]
+if (length(cargs)>0){
+	nffuncmat = cargs[1]
 }else{
-	metaghomedir = file.path(getwd(), 'oral_metagenomes')
+	nffuncmat = '~/oral_metagenomes/functional_analysis/ERP016024_IPR_abundances_v3.0.tsv'
 }
-stopifnot((file.exists(metaghomedir) && file.info(metaghomedir)$isdir))
+stopifnot((file.exists() && file.info(funcanaldir)$isdir))
 
 # load shared params and functions
-source('shared_params.r', local=TRUE)
+source('scripts/shared_params.r', local=TRUE)
 
 nbcores = 4
 
 options(width = 160)
 
-cargs = commandArgs(trailingOnly=T)
-nffuncmat = cargs[1]
-if (is.na(nffuncmat)){
-	
-#~ funcanaldir = '~/oral_metagenomes/functional_analysis'
-funcanaldir = file.path(metaghomedir, 'functional_analysis')
-nffuncmat = file.path(funcanaldir, 'ERP016024_IPR_abundances_v3.0.tsv')
 if (file.exists(nffuncmat)){
 	ffuncmat = file(nffuncmat)
 }else{
-	ffuncmat = file('https://www.ebi.ac.uk/metagenomics/projects/ERP016024/download/3.0/export?contentType=text&exportValue=IPR_abundances')
+	ebiurl = 'https://www.ebi.ac.uk/metagenomics/projects/ERP016024/download/3.0/export?contentType=text&exportValue=IPR_abundances'
+	cat(sprintf("Local file '%s' does not exist.\nInstead, download dataset from '%s'.\n"), nffuncmat, ebiurl)
+	ffuncmat = file(ebiurl)
+	nffuncmat = file.path(getwd(), 'ERP016024_IPR_abundances_v3.0.tsv') 
 }
 inmat = read.table(ffuncmat, header=T, row.names=1, sep='\t', quote='"')
 funcmat = data.matrix(t(inmat[,-1]))
 relfuncmat = t(apply(funcmat, 1, function(x){ x / sum(x) }))
 description = as.character(inmat[,1]) ; names(description) = rownames(inmat)
 
-ordsamfuncmat = order(rownames(funcmat))
-
-#~ plot(rowSums(funcmat[ordsamfuncmat,]), colSums(newfuncmat[,ordsamnewfuncmat]))
-
-individuals = sapply(rownames(relfuncmat), function(s){ as.character(sampleref[sampleref[['ENA_Run_ID']]==u, 'Sample'])
+individuals = sapply(rownames(relfuncmat), function(err){ as.character(sampleref[sampleref[['EBI_Metagenomics_Run_ID']]==err, 'Sample']) })
 	
 # all samples' metadata (from loaded script 'shared_params.r')
 criteria = getIndividualFactors(individual.labels=individuals)
 attach(criteria)
-strategy = ordered(names(lifestyles))
+
+pdf(sub('.tsv', '_counts_per_strategy.pdf', nffuncmat, fixed=T), height=12, width=20)
 
 # do a PCA to make dudi object
 func.pca = dudi.pca(relfuncmat, scannf=F, nf=ncol(relfuncmat), scale=F)
 # check how it is
 layout(matrix(1:2, 1, 2))
-s.class(func.pca$li, fac=strategy, xax=1, yax=2)	# PC1,2
-s.class(func.pca$li, fac=strategy, xax=3, yax=4)	# PC3,4
-# HG and AG look the same on PC 1-4 ... effect of sequencing?
+s.class(func.pca$li, fac=strategy, xax=1, yax=2, col=coullif)	# PC1,2
+s.class(func.pca$li, fac=strategy, xax=3, yax=4, col=coullif)	# PC3,4
 
 S = combn(nlevels(strategy),2)
-layout(matrix(1:(ncol(S)*2), ncol(S), 2, byrow=T))
-
 topdiscvars = list()
 topsignifdiscvars = list()
 topvars = list()
-#~ topdiscvars = apply(S, 2, function(s){
 for (i in 1:ncol(S)){
 	s = S[,i]
 	
@@ -77,10 +66,11 @@ for (i in 1:ncol(S)){
 #~ 	print(dim(pair.relfuncmat))
 	# do a PCA to make dudi object
 	pair.func.pca = dudi.pca(pair.relfuncmat, scannf=F, nf=nrow(pair.relfuncmat))
+	print(pair.func.pca$eig / sum(pair.func.pca$eig))
 #~ 	print(func.pca)
 	# check how it is
-	s.class(pair.func.pca$li, fac=strat, xax=1, yax=2)	# PC1,2
-	s.class(pair.func.pca$li, fac=strat, xax=3, yax=4)	# PC3,4
+	s.class(pair.func.pca$li, fac=strat, xax=1, yax=2, col=coullif[s])	# PC1,2
+	s.class(pair.func.pca$li, fac=strat, xax=3, yax=4, col=coullif[s])	# PC3,4
 	disc.func = discrimin(pair.func.pca, strat, scannf=F, nf=1)
 	topdiscvar = disc.func$va[order(abs(disc.func$va)[,1], decreasing=T),1]
 #~ 	print(topdiscvar[1:24])
@@ -110,15 +100,9 @@ for (i in 1:ncol(S)){
 	topvars[[pastepair]] = 	vardf	
 	topdiscvars[[pastepair]] = topdiscvar
 	topsignifdiscvars[[pastepair]] = topsignifdiscvar
-#~ 	return(topdiscvar)
-#~ })
 }
-#~ names(topvars) = names(topsignifdiscvars) = names(topdiscvars) = apply(S, 2, function(s){ paste(levels(strategy)[s], collapse='-') })
 
-# tentative synthesis of all pairwise comparisons
-#~ print(description[intersect(names(topdiscvars[[1]][1:100]), names(topdiscvars[[2]][1:100]))])
-#~ print(description[intersect(names(topdiscvars[[1]][1:100]), names(topdiscvars[[3]][1:100]))])
-#~ print(description[intersect(names(topdiscvars[[3]][1:100]), names(topdiscvars[[2]][1:100]))])
+dev.off()
 
 # sum the loadings (sum th signed loading so opposed varaitions will cancel each other)
 summary.pw.lda = as.data.frame(t(simplify2array(mclapply(1:dim(relfuncmat)[2], function(i){
@@ -161,13 +145,13 @@ if (length(cargs)>1){
 			return(rs/mean(rs))
 		}))
 		boxplot(wrelabun ~ strategy, ylab='weighted relative read abundance for combined pathway reactions', main=paste(ipterms, collapse=' '))
-#~ 		ggplot(data.frame(wrelabun=wrelabun, strategy=strategy), aes(strategy, wrelabun))+geom_boxplot()+ylab('weighted relative read abundance for combined pathway reactions')+ggtitle(paste(ipterms, collapse=' '))+geom_jitter()
+# 		ggplot(data.frame(wrelabun=wrelabun, strategy=strategy), aes(strategy, wrelabun))+geom_boxplot()+ylab('weighted relative read abundance for combined pathway reactions')+ggtitle(paste(ipterms, collapse=' '))+geom_jitter()
 		layout(matrix(1:9,3,3,byrow=T))
 		for (ipterm in ipterms){
 			boxplot(relfuncmat[, ipterm] ~ strategy, ylab='relative read abundance for functional annotation',
 			 main=paste(paste(ipterm, as.character(candidatepathway[ipterm,1]), sep=' - '), description[ipterm], sep='\n'))
 		}
-#~ 		ggplot(as.data.frame(cbind(relfuncmat[, ipterms], strategy)), aes(strategy))+geom_boxplot()+ylab('relative read abundance for functional annotation')+ggtitle(paste(paste(ipterm, as.character(candidatepathway[ipterm,1]), sep=' - '), description[ipterm], sep='\n'))+facet_grid(.~ipterms)
+# 		ggplot(as.data.frame(cbind(relfuncmat[, ipterms], strategy)), aes(strategy))+geom_boxplot()+ylab('relative read abundance for functional annotation')+ggtitle(paste(paste(ipterm, as.character(candidatepathway[ipterm,1]), sep=' - '), description[ipterm], sep='\n'))+facet_grid(.~ipterms)
 		dev.off()
 	}	
 }
